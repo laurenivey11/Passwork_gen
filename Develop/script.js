052e35c319d747d37cd3205e5dc9263a32f63dd4
Lauren Ivey <lauren.ivey11@gmail.com>
2021-01-14 20:30:18 -0500

    HTML and CSS updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,73 +1,11 @@
 // Assignment code here
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(getRandomNumber());
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 9);
-}
-
-function getRandomSymbol() {
-  var symbol = "!@#$%^&*(){}[]=<>/,.~?";
-  return symbol[Math.floor(Math.random()*symbol.length)];
-}
-
-
-
-
-function generatePassword(upper, lower, number, symbol, length){
-  let generatedPassword = "";
-
-  const typesCount = upper + lower + number + symbol;
-
-  //console.log(typesCount);
-
-  const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-
-  if(typesCount === 0) {
-      return '';
-  }
-
-  for(let i=0; i<length; i+=typesCount) {
-      typesArr.forEach(type => {
-          const funcName = Object.keys(type)[0];
-          generatedPassword += randomFunc[funcName]();
-      });
-  }
-
-  const finalPassword = generatedPassword.slice(0, length);
-
-
-  return finalPassword;
-}
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var lengthEl = document.getElementById("length");
-var numberEl = document.getElementById("number");
-var lowerEl = document.getElementById("lower");
-var upperEl = document.getElementById("upper");
-var symbolEl = document.getElementById("symbol");
 
 // Write password to the #password input
-function writePassword(lower, upper, number, symbol) {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -76,13 +14,4 @@ function writePassword(lower, upper, number, symbol) {
 }
 
 // Add event listener to generate button
-
-generateBtn.addEventListener('click', () =>{
-  const length = +lengthEl.value;
-  const hasUpper = upperEl.checked;
-  const hasLower = lowerEl.checked;
-  const hasNumber = numberEl.checked;
-  const hasSymbol = symbolEl.checked;
-
-answerEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
-});
+generateBtn.addEventListener("click", writePassword);
